b6591d5a67f04617a8f36980171b158cc55f9e09
Rafael Carter <Rafael.Carter@cosairus.com>
2021-03-06 16:39:34 -0500

    Initial Commit

---
+++ NetflixClone/NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Rafael Carter on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
