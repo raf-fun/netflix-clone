cc8eac9e1c3b673ba1b0afd9a1b5c4ea08267bb4
Rafael Carter <Rafael.Carter@cosairus.com>
2021-04-04 21:20:43 -0400

    Initial Commit

---
+++ netflix-clone/netflix-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  netflix-clone
+//
+//  Created by Rafael Carter on 4/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netflix-clone/netflix-clone/netflix_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflix_cloneApp.swift
+//  netflix-clone
+//
+//  Created by Rafael Carter on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct netflix_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
